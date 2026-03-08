1d5df81d065a4c5df237f5f95d184e6f56e1fbe4
yuri_rodnov <somewaystodo@yandex.ru>
2026-03-09 01:03:22 +0300

    add first code

--- 003_snake/src/entities/Background.ts
+++ 003_snake/src/entities/Background.ts
@@ -3,7 +3,9 @@
 export class Background {
   private image: HTMLImageElement;
 
-  constructor(img: HTMLImageElement) {}
+  constructor(img: HTMLImageElement) {
+    this.image = img;
+  }
 
   draw(ctx: CanvasRenderingContext2D) {}
 }
