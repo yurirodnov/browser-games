b6fceb13d0e3bd28f75c857aaa69edc0f639d953
yuri_rodnov <somewaystodo@yandex.ru>
2026-04-13 22:34:13 +0300

    add brick code

--- 005_tetris/src/entities/Brick.ts
+++ 005_tetris/src/entities/Brick.ts
@@ -3,7 +3,6 @@ import type { BrickColor, BricksAssets } from "../types/types";
 export class Brick {
   private images: BricksAssets;
   private currentImage: HTMLImageElement;
-  private color: BrickColor;
 
   private coordX: number;
   private coordY: number;
@@ -13,12 +12,16 @@ export class Brick {
 
   constructor(images: BricksAssets, color: BrickColor, x: number, y: number, size: number) {
     this.images = images;
-    this.color = color;
+    this.currentImage = this.images[color];
 
     this.coordX = x;
     this.coordY = y;
 
     this.width = size;
     this.height = size;
+  }
+
+  public draw(ctx: CanvasRenderingContext2D): void {
+    ctx.drawImage(this.currentImage, this.coordX, this.coordY, this.width, this.height);
   }
 }
